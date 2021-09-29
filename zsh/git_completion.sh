b1d6e72c1c287ab778a3fce0d44ece4bd8320dcb
Carlos Valle <carlos.m.valle@gmail.com>
2021-09-29 14:47:28 -0400

    Remove old config

--- zsh/git_completion.sh
+++
@@ -1,6 +0,0 @@
-# this is specific to the location of the current version of git, installed by homebrew
-completion='$(brew --prefix)/share/zsh/site-functions/_git'
-
-if test -f $completion; then
-    source $completion
-fi
